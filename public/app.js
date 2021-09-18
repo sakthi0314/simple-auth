a475589431a95c8dace8d6a9c15f85216cfbb56e
sakthi0314 <rsremosakthi@gmail.com>
2021-09-18 06:55:14 -0700

    success

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ axios.defaults.withCredentails = true
 const form = document.getElementById("form")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
+const title = document.getElementById("title")
 
 form.addEventListener("submit", e => {
   e.preventDefault()
@@ -12,7 +13,11 @@ form.addEventListener("submit", e => {
     password: password.value,
   }
 
-  axios.post("/login", creds).then(data => {
-    console.log(data)
+  axios.post("/login", creds).then(res => {
+    title.innerText = res.data.message
+
+    if (res.data.message) {
+      form.style.display = "none"
+    }
   })
 })
